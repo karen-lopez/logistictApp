04f6e1ab9cbca0344d5b7898989352c285db2510
Karen Lopez <stefanny@utp.edu.co>
2022-03-19 17:36:50 -0500

    start project

---
+++ src/main/java/com/kl/logisticts/LogistictsApplication.java
@@ -0,0 +1,13 @@
+package com.kl.logisticts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogistictsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogistictsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kl/logisticts/LogistictsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kl.logisticts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogistictsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
