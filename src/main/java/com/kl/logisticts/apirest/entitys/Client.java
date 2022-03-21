a88a383ea63a959c643014d38b92c7e6112e364d
Karen Lopez <stefanny@utp.edu.co>
2022-03-20 21:47:23 -0500

    creation of data validations

--- src/main/java/com/kl/logisticts/apirest/entitys/Client.java
+++ src/main/java/com/kl/logisticts/apirest/entitys/Client.java
@@ -1,20 +1,29 @@
 package com.kl.logisticts.apirest.entitys;
 
 import org.springframework.data.mongodb.core.mapping.Document;
-
+import static com.kl.logisticts.apirest.validations.Validation.validateNotNull;
 import org.springframework.data.annotation.Id;
-
-import lombok.Data;
-import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 
 @Document
-@Data
-@AllArgsConstructor
+@Getter
 public class Client{
     @Id
     private String id;
     private Long documentNumber; 
     private String name;
+    
+    private static final String CLIENT_DOCUMENT_NUMBER_IS_REQUIRED = "Client document number is required.";
+    private static final String CLIENT_NAME_IS_REQUIRED = "Client name is required.";
 
+    
+    public Client(Long documentNumber, String name) {
+        validateNotNull(documentNumber, CLIENT_DOCUMENT_NUMBER_IS_REQUIRED);
+        validateNotNull(name, CLIENT_NAME_IS_REQUIRED);
+        this.documentNumber = documentNumber;
+        this.name = name;
+    }
+
+    
 }
--- src/main/java/com/kl/logisticts/apirest/entitys/delivery/RoadDelivery.java
+++ src/main/java/com/kl/logisticts/apirest/entitys/delivery/RoadDelivery.java
@@ -1,6 +1,6 @@
 package com.kl.logisticts.apirest.entitys.delivery;
 
-
+import static com.kl.logisticts.apirest.validations.Validation.validateNotNull;
 import com.kl.logisticts.apirest.entitys.transport.ITransport;
 import com.kl.logisticts.apirest.entitys.transport.Vehicle;
 import java.math.BigDecimal;
@@ -13,10 +13,12 @@ public final class RoadDelivery extends Delivery{
     private String warehouse;
     private String licensePlate;
     private static final Integer DISCOUNT_RATE = 2;
+    private static final String DELIVERY_WAREHOUSE_IS_REQUIRED = "Delivery wareHouse is required.";
 
     public RoadDelivery(String warehouse, String productType, Integer totalProduct, Date registrationDate, Date deliveryDate, BigDecimal price, String trackingNumber, String clientId, String licensePlate) {
         super(productType, totalProduct, registrationDate, deliveryDate, price, trackingNumber, clientId);
         validateDiscount();
+        validateNotNull(warehouse, DELIVERY_WAREHOUSE_IS_REQUIRED);
         this.warehouse = warehouse;
         this.licensePlate = licensePlate;
         this.transport = createTransport();
